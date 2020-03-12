47c16df141222844ea28adb49520d1293c244d88
shubhamdeulkar27 <shubhamdeulkar27@gmail.com>
2020-03-12 17:33:09 +0530

    Added:-Sum and Average of 5 Random Numbers

---
+++ Sequences Practice/random_five.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -x
+
+ran1=$((10+RANDOM%100))
+ran2=$((10+RANDOM%100))
+ran3=$((10+RANDOM%100))
+ran4=$((10+random%100))
+ran5=$((10+random%100))
+
+sum=$(($ran1+$ran2+$ran3+$ran4+$ran5))
+avg=$(($sum/5))
