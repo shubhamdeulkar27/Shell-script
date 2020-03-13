f41cc6020fa4230dec05a7ef9b485bf8b55b2453
shubhamdeulkar27 <shubhamdeulkar27@gmail.com>
2020-03-13 15:06:08 +0530

    Added: Determining File Extension

---
+++ Selection Statements/Case Statements/case_file_type.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -x
+
+	for filename in $(ls)
+	do
+		#TAKE THE EXTENTION
+		ext=${filename##*\.}
+		case "$ext" in
+		java) echo "$filename : Java Source File"
+				;;
+		o)	echo "$filename : object File"
+			;;
+		sh) echo "$filename : Shell Script"
+			;;
+		txt) echo "$filename : Text File"
+			;;
+		*)	echo "$filename : Not Processed"
+			;;
+		esac
+	done
