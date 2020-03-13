667cc0828c8efbfac93e5ad4681f295c1eddd5c6
shubhamdeulkar27 <shubhamdeulkar27@gmail.com>
2020-03-13 19:41:42 +0530

    Refactored: Indentation

--- Selection Statements/If Elif/week_day.sh
+++ Selection Statements/If Elif/week_day.sh
@@ -1,27 +1,28 @@
 #!/bin/bash -x
 
-	read -p "Enter the Number" day
-	if [ $day -eq 0 ]
-	then
-		printf "Sunday\n"
-	elif [ $day -eq 1 ]
-	then
-		printf "Monday\n"
-	elif [ $day -eq 2 ]
-	then
-		printf "Tuesday\n"
-	elif [ $day -eq 3 ]
-	then
-		printf "Wednesday\n"
-	elif [ $day -eq 4 ]
-	then
-		printf "Thursday\n"
-	elif [ $day -eq 5 ]
-	then
-		printf "Friday\n"
-	elif [ $day -eq 6 ]
-	then
-		printf "Saturday"
-	else
-		printf "Ivalid Input\n"
-	fi
+read -p "Enter the Number" day
+
+if [ $day -eq 0 ]
+then
+	printf "Sunday\n"
+elif [ $day -eq 1 ]
+then
+	printf "Monday\n"
+elif [ $day -eq 2 ]
+then
+	printf "Tuesday\n"
+elif [ $day -eq 3 ]
+then
+	printf "Wednesday\n"
+elif [ $day -eq 4 ]
+then
+	printf "Thursday\n"
+elif [ $day -eq 5 ]
+then
+	printf "Friday\n"
+elif [ $day -eq 6 ]
+then
+	printf "Saturday"
+else
+	printf "Ivalid Input\n"
+fi
