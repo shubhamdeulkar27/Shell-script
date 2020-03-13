337f5ad6cdafea142ce096052a04023fbb53ab32
shubhamdeulkar27 <shubhamdeulkar27@gmail.com>
2020-03-13 16:45:14 +0530

    Added: Unit Conversion Using Case

---
+++ Selection Statements/Case Statements/unit_conversion.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -x
+
+	read -p "Enter the Data : " data
+	printf "Enter\n1 Feet to Inch\n2 Feet to Meter\n3 Inch to Feet\n4 Meter to Feet\n"
+	read -p "Enter Operation : " op
+
+	case $op in
+				1)
+				res=$(($data*12))
+				echo "$res Inch"
+				;;
+				2)
+				res=`echo "$data * 0.3048"|bc`
+				echo "$res Meter"
+				;;
+				3)
+				res=`echo "$data * 0.0803030"|bc`
+				echo "$res Feet"
+				;;
+				4)
+				res=`echo "$data * 0.3048"|bc`
+				echo "$res Feet"
+				;;
+				*)
+				echo "Invalid Option"
+				;;
+	esac
