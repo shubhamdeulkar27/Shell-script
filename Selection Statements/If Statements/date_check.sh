c978a5d4a9a409957f8cfed081c3978c8b0ea524
shubhamdeulkar27 <shubhamdeulkar27@gmail.com>
2020-03-13 15:26:51 +0530

    Refactored: JUNE 20 Updated

---
+++ Selection Statements/If Statements/date_check.sh
@@ -0,0 +1,58 @@
+#!/bin/bash -x
+
+	read -p "Enter the Day : " day
+	read -p "Enter the Month : " month
+	MARCH=3
+	APRIL=4
+	MAY=5
+	JUNE=6
+	if [ $month -ge $MARCH -a $month -le $JUNE ]
+	then
+		#FOR MARCH
+		if [ $month -eq $MARCH ]
+		then
+			if [ $day -ge 20 -a $day -le 31 ]
+			then
+				printf "True\n"
+			else
+				printf "False\n"
+			fi
+
+		#FOR APRIL
+		elif [ $month -eq $APRIL ]
+		then
+			if [ $day -ge 1 -a $day -le 30 ]
+			then
+				printf "True\n"
+			else
+				printf "False\n"
+			fi
+
+		#FOR MAY
+		elif [ $month -eq $MAY ]
+		then
+			if [ $day -ge 1 -a $day -le 31 ]
+			then
+				printf "True\n"
+			else
+				printf "False\n"
+			fi
+
+		#FOR JUNE
+		elif [ $month -eq $JUNE ]
+		then
+			if [ $day -ge 1 -a $day -le 20 ]
+			then 
+				printf "True\n"
+			else
+				printf "False\n"
+			fi
+
+		else
+			printf "Invalid Input\n"
+		fi
+	else
+		printf "Invalid Input\n"
+	fi
+
+
