979e0b7875b1544ee6daa1bed42208470d43b0e2
shubhamdeulkar27 <shubhamdeulkar27@gmail.com>
2020-03-13 19:50:51 +0530

    Refactored: Indentation

--- Selection Statements/If Statements/employee_wage.sh
+++ Selection Statements/If Statements/employee_wage.sh
@@ -1,17 +1,18 @@
 #!/bin/bash -x
 
-	PARTTIME=1
-	FULLTIME=2
-	empRatePerHour=20
-	randomEmp=$((RANDOM%3))
-	if [ $randomEmp -eq $PARTTIME ]
-	then
-		empHrs=4
-	elif [ $randomEmp -eq $FULLTIME ]
-	then
-		empHrs=8
-	else
-		empHrs=0
-	fi
+PARTTIME=1
+FULLTIME=2
+empRatePerHour=20
+randomEmp=$((RANDOM%3))
 
-	Salary=$(($empRatePerHour*$empHrs))
+if [ $randomEmp -eq $PARTTIME ]
+then
+	empHrs=4
+elif [ $randomEmp -eq $FULLTIME ]
+then
+	empHrs=8
+else
+	empHrs=0
+fi
+
+Salary=$(($empRatePerHour*$empHrs))
