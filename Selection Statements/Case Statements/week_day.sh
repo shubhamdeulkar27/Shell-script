671d987da9ce6743cfcdaba4b0e5c256191b77f9
shubhamdeulkar27 <shubhamdeulkar27@gmail.com>
2020-03-13 19:28:03 +0530

    Refactored: Indentation

--- Selection Statements/Case Statements/week_day.sh
+++ Selection Statements/Case Statements/week_day.sh
@@ -1,29 +1,30 @@
 #!/bin/bash -x
 
-	read -p "Enter the Number : " day
-	case $day in
-			0)
-				echo "Sunday"
-			;;
-			1)
-				echo "Monday"
-			;;
-			2)
-				echo "Tuesday"
-			;;
-			3)
-				echo "Wednesday"
-			;;
-			4)
-				echo "Thursday"
-			;;
-			5)
-				echo "Friday"
-			;;
-			6)
-				echo "Saturday"
-			;;
-			*)
-				echo "Invalid Input"
-			;;
-	esac
+read -p "Enter the Number : " day
+
+case $day in
+	0)
+	echo "Sunday"
+	;;
+	1)
+	echo "Monday"
+	;;
+	2)
+	echo "Tuesday"
+	;;
+	3)
+	echo "Wednesday"
+	;;
+	4)
+	echo "Thursday"
+	;;
+	5)
+	echo "Friday"
+	;;
+	6)
+	echo "Saturday"
+	;;
+	*)
+	echo "Invalid Input"
+	;;
+esac
