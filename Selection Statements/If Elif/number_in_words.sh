ff7b01e0c99d493b53c178e5df06dd964e1e174f
shubhamdeulkar27 <shubhamdeulkar27@gmail.com>
2020-03-13 11:38:22 +0530

    Added: One Digit Number Into Word

---
+++ Selection Statements/If Elif/number_in_words.sh
@@ -0,0 +1,36 @@
+#!/bin/bash -x
+
+	read -p "Enter any One Digit Number : " num
+	if [ $num -eq 0 ]
+	then
+		printf "Zero\n"
+	elif [ $num -eq 1 ]
+	then
+		printf "One\n"
+	elif [ $num -eq 2 ]
+	then
+		printf "Two\n"
+	elif [ $num -eq 3 ]
+	then
+		printf "Three\n"
+	elif [ $num -eq 4 ]
+	then
+		printf "Four\n"
+	elif [ $num -eq 5 ]
+	then
+		printf "Five\n"
+	elif [ $num -eq 6 ]
+	then
+		printf "Six\n"
+	elif [ $num -eq 7 ]
+	then
+		printf "Seven\n"
+	elif [ $num -eq 8 ]
+	then
+		printf "Eight\n"
+	elif [ $num -eq 9 ]
+	then
+		printf "Nine\n"
+	else
+		printf "Invalid Input\n"
+	fi
