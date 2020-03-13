4ca36d991b162297942b3ae98243009455617211
shubhamdeulkar27 <shubhamdeulkar27@gmail.com>
2020-03-13 19:57:12 +0530

    Refactored: Indentation

--- Selection Statements/If Statements/leap_year.sh
+++ Selection Statements/If Statements/leap_year.sh
@@ -1,20 +1,20 @@
 #!/bin/bash -x
 	
-	read -p "Enter the Year : "  year
-	res=0	
-	if [ $(($year%4)) -eq 0 -a $(($year%100)) -ne 0 ]
-	then
-		res=1
-	elif [ $(($year%400)) -eq 0 ]
-	then 
-		res=1
-	else
-		res=0
-	fi
+read -p "Enter the Year : "  year
+res=0	
+if [ $(($year%4)) -eq 0 -a $(($year%100)) -ne 0 ]
+then
+	res=1
+elif [ $(($year%400)) -eq 0 ]
+then 
+	res=1
+else
+	res=0
+fi
 
-	if [ $res -eq 1 ]
-	then
-		printf "It is A Leap Year\n"
-	else
-		printf "It is Not A Leap Year\n"
-	fi
+if [ $res -eq 1 ]
+then
+	printf "It is A Leap Year\n"
+else
+	printf "It is Not A Leap Year\n"
+fi
