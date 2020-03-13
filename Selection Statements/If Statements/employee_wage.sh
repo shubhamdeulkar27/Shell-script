e45222408c91269db3854f03a7c278c3be7559e3
shubhamdeulkar27 <shubhamdeulkar27@gmail.com>
2020-03-13 11:19:38 +0530

    Added: Calculates Employee's Salary Per Hr

---
+++ Selection Statements/If Statements/employee_wage.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -x
+
+	PARTTIME=1
+	FULLTIME=2
+	empRatePerHour=20
+	randomEmp=$((RANDOM%3))
+	if [ $randomEmp -eq $PARTTIME ]
+	then
+		empHrs=4
+	elif [ $randomEmp -eq $FULLTIME ]
+	then
+		empHrs=8
+	else
+		empHrs=0
+	fi
+
+	Salary=$(($empRatePerHour*$empHrs))
