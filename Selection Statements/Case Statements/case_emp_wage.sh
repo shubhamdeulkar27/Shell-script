a24a2f5c2a74b0d5a67cfa68cd99775b6512009b
shubhamdeulkar27 <shubhamdeulkar27@gmail.com>
2020-03-13 19:13:07 +0530

    Refactored: Indentation

--- Selection Statements/Case Statements/case_emp_wage.sh
+++ Selection Statements/Case Statements/case_emp_wage.sh
@@ -1,19 +1,18 @@
 #!/bin/bash -x
 
-	PARTTIME=1
-	FULLTIME=2
-	empRate=20
-	empCheck=$((RANDOM%3))
-	case $empCheck in
-		$FULLTIME)
-					empHrs=8
-					;;
-		$PARTTIME)
-					empHrs=4
-					;;
+PARTTIME=1
+FULLTIME=2
+empRate=20
+empCheck=$((RANDOM%3))
+case $empCheck in
+	$FULLTIME)
+		empHrs=8
+		;;
+	$PARTTIME)
+		empHrs=4
+		;;
 		*)
 		empHrs=0
-					;;
-	esac
-
-	salary=$(($empRate*$empHrs))
+		;;
+esac
+salary=$(($empRate*$empHrs))
