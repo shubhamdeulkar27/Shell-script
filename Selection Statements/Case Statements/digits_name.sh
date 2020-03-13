d2771fb72c40ce9245963c16e04d2d75a934f583
shubhamdeulkar27 <shubhamdeulkar27@gmail.com>
2020-03-13 15:56:34 +0530

    Added: Digits Name Using Case

---
+++ Selection Statements/Case Statements/digits_name.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -x
+
+	read -p "Enter Number : " num
+	case $num in
+				1)
+				echo "Unit"
+				;;
+				10)
+				echo "Ten"
+				;;
+				100)
+				echo "Hundred"
+				;;
+				1000)
+				echo "Thousand"
+				;;
+				100000)
+				echo "Lks"
+				;;
+				*)
+				echo "Unknown"
+				;;
+	esac
