021895000134d2d3b2e833160669c0a8a1820b10
shubhamdeulkar27 <shubhamdeulkar27@gmail.com>
2020-03-13 19:43:55 +0530

    Refactored: Indentation

--- Selection Statements/If Statements/coin_flip.sh
+++ Selection Statements/If Statements/coin_flip.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -x
 
-	coin=$((RANDOM%2))
-	HEAD=0
-	TAIL=1
-	if [ $coin -eq	$HEAD  ]
-	then
-		printf "Heads\n"
-	elif [ $coin -eq $TAIL ]
-	then
-		printf "Tails\n"
-	fi
+coin=$((RANDOM%2))
+HEAD=0
+TAIL=1
+if [ $coin -eq	$HEAD  ]
+then
+	printf "Heads\n"
+elif [ $coin -eq $TAIL ]
+then
+	printf "Tails\n"
+fi
