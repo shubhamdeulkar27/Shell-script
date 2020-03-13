f2e79ed42cc3fe1051aad25736b8eab0a11d69df
shubhamdeulkar27 <shubhamdeulkar27@gmail.com>
2020-03-13 19:03:16 +0530

    Refactored: Indentation

--- Unit Conversion/ft-meter-acres.sh
+++ Unit Conversion/ft-meter-acres.sh
@@ -1,15 +1,15 @@
 #!/bin/bash -x
 
-#Feet to Meter
+
 read -p "Enter L in feet " l
 read -p "Enter W in feet " w
 
+#FEET TO METER
 len=`echo "$l * 0.3048"|bc `
 wed=`echo "$w * 0.3048"|bc`
 echo Plot is of $len x $wed
 
-
-#Meter to Acres
+#METER TO ACRES
 sqMeter=`echo "$len * $wed"|bc`
 acres=`echo "$sqMeter * 0.000247105"|bc`
 
