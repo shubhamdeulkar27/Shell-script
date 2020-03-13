2a6f2814741ac64744abc31ad7dc4f0db41edf3e
shubhamdeulkar27 <shubhamdeulkar27@gmail.com>
2020-03-13 19:17:37 +0530

    Refactored: Indentation

--- Selection Statements/Case Statements/digits_name.sh
+++ Selection Statements/Case Statements/digits_name.sh
@@ -1,23 +1,23 @@
 #!/bin/bash -x
 
-	read -p "Enter Number : " num
-	case $num in
-				1)
-				echo "Unit"
-				;;
-				10)
-				echo "Ten"
-				;;
-				100)
-				echo "Hundred"
-				;;
-				1000)
-				echo "Thousand"
-				;;
-				100000)
-				echo "Lks"
-				;;
-				*)
-				echo "Unknown"
-				;;
-	esac
+read -p "Enter Number : " num
+case $num in
+	1)
+	echo "Unit"
+	;;
+	10)
+	echo "Ten"
+	;;
+	100)
+	echo "Hundred"
+	;;
+	1000)
+	echo "Thousand"
+	;;
+	100000)
+	echo "Lks"
+	;;
+	*)
+	echo "Unknown"
+	;;
+esac
