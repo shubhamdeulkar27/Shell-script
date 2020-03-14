5f1160eccd67441346354128531bc493b3a0bbb4
shubhamdeulkar27 <shubhamdeulkar27@gmail.com>
2020-03-14 09:00:41 +0530

    Refactored: Indentation

--- Selection Statements/Repetation Statements/powers_two.sh
+++ Selection Statements/Repetation Statements/powers_two.sh
@@ -1,11 +1,12 @@
 #!/bin/bash -x
 
-	read -p "Enter The Number : " n
-	res=1;
-	MP=2;
-	for (( count=1 ; count<=n ; count++ ))
-	do
-		res=$(($res*$MP))
-		printf "$res "
-	done
-	printf "\n"
+read -p "Enter The Number : " n
+res=1;
+MP=2;
+
+for (( count=1 ; count<=n ; count++ ))
+do
+	res=$(($res*$MP))
+	printf "$res "
+done
+printf "\n"
