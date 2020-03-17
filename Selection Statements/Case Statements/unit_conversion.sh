23cf0d2fa1255f69ce7e0d8c7a27b7881139b84c
shubhamdeulkar27 <shubhamdeulkar27@gmail.com>
2020-03-17 18:31:30 +0530

    Refactored: Indentation

--- Selection Statements/Case Statements/unit_conversion.sh
+++ Selection Statements/Case Statements/unit_conversion.sh
@@ -7,22 +7,22 @@ read -p "Enter Data : " data
 
 case $op in
 	1)
-	res=$(($data*12))
-	echo "$res Inch"
+		res=$(($data*12))
+		echo "$res Inch"
 	;;
 	2)
-	res=`echo "$data * 0.3048"|bc`
-	echo "$res Meter"
+		res=`echo "$data * 0.3048"|bc`
+		echo "$res Meter"
 	;;
 	3)
-	res=`echo "$data * 0.0803030"|bc`
-	echo "$res Feet"
+		res=`echo "$data * 0.0803030"|bc`
+		echo "$res Feet"
 	;;
 	4)
-	res=`echo "$data * 0.3048"|bc`
-	echo "$res Feet"
+		res=`echo "$data * 0.3048"|bc`
+		echo "$res Feet"
 	;;
 	*)
-	echo "Invalid Option"
+		echo "Invalid Option"
 	;;
 esac
