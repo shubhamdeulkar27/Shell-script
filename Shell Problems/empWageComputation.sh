f35283351b10df67f0aaad920ce0eecd07a76345
shubhamdeulkar27 <shubhamdeulkar27@gmail.com>
2020-03-18 10:52:25 +0530

    Added: Attendence Feature

--- Shell Problems/empWageComputation.sh
+++ Shell Problems/empWageComputation.sh
@@ -1,3 +1,14 @@
 #!/bin/bash -x
 
 printf "Welcome to Employee Wage Computation Program\n"
+
+isPresent=1
+randomCheck=$(( RANDOM%2 ))
+
+if (( $randomCheck==$isPresent ))
+then
+	printf "Employee Is Present"
+else
+	printf "Employee is Not Present"
+fi
+
