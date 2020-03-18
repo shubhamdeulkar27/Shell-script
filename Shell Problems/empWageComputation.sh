59af2eca714f649afd3f81d09cbb8dfab756c06b
shubhamdeulkar27 <shubhamdeulkar27@gmail.com>
2020-03-18 13:52:42 +0530

    Added: Used Case Statement for Employee Hours

--- Shell Problems/empWageComputation.sh
+++ Shell Problems/empWageComputation.sh
@@ -2,23 +2,21 @@
 
 printf "Welcome to Employee Wage Computation Program\n"
 
-absent=0
 isPartTime=1
 isFullTime=2
 empRatePerHour=20
 randomCheck=$(( RANDOM%3 ))
 
-if (( $absent==$randomCheck ))
-then
-		salary=0
-
-elif (( $isPartTime==$randomCheck ))
-then
+case $randomCheck in
+	$isPartTime)
 		empHours=4
-		salary=$(( $empHours*$empRatePerHour ))
-
-elif (( $isFullTime==$randomCheck ))
-then
+	;;
+	$isFullTime)
 		empHours=8
-		salary=$(( $empHours*$empRatePerHour ))
-fi
+	;;
+	*)
+		empHours=0
+	;;
+esac
+
+salary=$(( $empRatePerHour*$empHours ))
