56bae7d2dc7d9acaadbb9eedc7daf9ad6577f272
shubhamdeulkar27 <shubhamdeulkar27@gmail.com>
2020-03-18 14:08:51 +0530

    Added: Wages Of Month Feature

--- Shell Problems/empWageComputation.sh
+++ Shell Problems/empWageComputation.sh
@@ -5,18 +5,24 @@ printf "Welcome to Employee Wage Computation Program\n"
 isPartTime=1
 isFullTime=2
 empRatePerHour=20
-randomCheck=$(( RANDOM%3 ))
+numberOfWorkingDays=20
+totalSalary=0
 
-case $randomCheck in
-	$isPartTime)
-		empHours=4
-	;;
-	$isFullTime)
-		empHours=8
-	;;
-	*)
-		empHours=0
-	;;
-esac
+for (( day=1 ; day<=$numberOfWorkingDays ; day++ ))
+do
+	randomCheck=$(( RANDOM%3 ))
+		case $randomCheck in
+			$isPartTime)
+				empHours=4
+			;;
+			$isFullTime)
+				empHours=8
+			;;
+			*)
+				empHours=0
+			;;
+		esac
+	salary=$(( $empRatePerHour*$empHours ))
+	totalSalary=$(($totalSalary+$salary))
+done
 
-salary=$(( $empRatePerHour*$empHours ))
