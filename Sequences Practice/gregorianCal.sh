3136345f1fd9a57adda37ef694c766559cea7b2d
shubhamdeulkar27 <shubhamdeulkar27@gmail.com>
2020-03-19 16:04:13 +0530

    Refactored: Updates Done

--- Sequences Practice/gregorianCal.sh
+++ Sequences Practice/gregorianCal.sh
@@ -4,7 +4,35 @@ read -p "Enter the Month : " m
 read -p "Enter the Day : " d
 read -p "Enter the Year : " y
 
-yo=$(($(($y-$((14-$m))))/12))
+yo=$(($y-$((14-$m))/12))
 x=$(($yo+$(($yo/4))-$(($yo/100))+$(($yo/400))))
-mo=$(($m+12*$(($((14-m))/12))-2))
+mo=$(($m+12*$(($((14-$m))/12))-2))
 d0=$(($(($d+$x+31*$mo/12))%7))
+
+case $d0 in
+	0)
+		echo "Sunday"
+	;;
+	1)
+		echo "Monday"
+	;;
+	2)
+		echo "Tuesday"
+	;;
+	3)
+		echo "Wednesday"
+	;;
+	4)
+		echo "Thursday"
+	;;
+	5)
+		echo "Friday"
+	;;
+	6)
+		echo "Saturday"
+	;;
+	*)
+		echo "Invalid"
+	;;
+esac
+
